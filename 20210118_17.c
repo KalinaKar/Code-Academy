9a69dc46c6fe52b9f43497f93438c1a29677fa35
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-01-19 00:28:11 +0200

    Update 20210118_17.c

--- 20210118_17.c
+++ 20210118_17.c
@@ -7,14 +7,15 @@ int main(){
     printf("%d\n", generator());
     printf("%d\n", generator());
     printf("%d\n", generator());
+
     return 0;
 }
 
 long long int generator(){
     static long long int x = 1;
-	x ^= x << 13;
-	x ^= x >> 17;
-	x ^= x << 5;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
 
-	return x;
+    return x;
 }
