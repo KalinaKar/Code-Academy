0d5041ad80de3e7e005d0b9fd65232b3c148208e
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-01-26 14:55:30 +0200

    Update 20210125_1.c

--- 20210125_1.c
+++ 20210125_1.c
@@ -1,11 +1,13 @@
+/*Пребройте символите, подавани на конзолата, с
+функцията getchar();*/
 #include <stdio.h>
 
 int main(){
     char c;
-    c = getchar();
-    while(c != EOF){
-        putchar(c);
-        c = getchar();
-    }
+    int counter = 0;
+    while ((c = getchar()) != EOF){
+        ++counter;       
+    }  
+    printf("Number of chars: %d\n", counter);
     return 0;
 }
