5fcff79258ab820db28c0708d85878d898e75cca
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-03-13 22:34:25 +0200

    Update 20210308_19.c

--- 2021.03.08/20210308_19.c
+++ 2021.03.08/20210308_19.c
@@ -12,13 +12,13 @@ int main(int argc, char* argv[]){
         int i;
         for(i = 1; i < argc; i++){
             char *filename = argv[i];
-            FILE *pfile = NULL;
+            FILE *pfile = NULL;            
+            pfile = fopen(filename, "rt");
             if (pfile == NULL){
                 perror("Failed to open file\n");
                 exit(1);
             }
-            pfile = fopen(filename, "rt");
-            printf("\n\n%s content:\n", filename);            
+            printf("\n\nPage: %d\t%s content:\n", i, filename);            
             while((c = fgetc(pfile)) != EOF){
                 putchar(c);
             }
