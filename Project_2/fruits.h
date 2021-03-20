f41944ce1c882f9a35acadff7f7d69e9245cca92
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-03-21 01:37:51 +0200

    Update fruits.h

--- Project_2/fruits.h
+++ Project_2/fruits.h
@@ -4,6 +4,10 @@
 #define K 1000
 
 void calcFruitWeight(char *fileIn, char *fileOut);
+    /* takes the names of the input and output files and manages the other funcitions */
 FILE *openFile(char *filename, char *mode);
+    /* returns a pointer to an open file or exits the program if file does not open */
 int *subsetSum(int *a, int n);
+    /* calculates subset of sum and returns a pointer to it if one is possible */
 void memoryCheck(int *p);
+    /* aborts program if p holds NULL */
